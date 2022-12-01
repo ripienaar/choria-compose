a31c23c83adefda59f53a0d31577b60349955c90
R.I.Pienaar <rip@devco.net>
2022-12-01 14:27:39 +0100

    enable server side jwt authentication
    Signed-off-by: R.I.Pienaar <rip@devco.net>

---
+++ config/aaa/admin.rego
@@ -0,0 +1,3 @@
+package io.choria.aaasvc
+
+default allow = true
--- setup.sh
+++ setup.sh
@@ -58,7 +58,7 @@ cp credentials/ca/ca.crt credentials/broker
 cat config/broker/broker.templ|sed -e "s.ISSUER.$(cat credentials/issuer/issuer.public)." > config/broker/broker.conf
 
 log "Creating provisioning.jwt"
-choria jwt prov credentials/server/provisioning.jwt credentials/issuer/issuer.seed --token s3cret --urls nats://broker.choria.local:4222 --default --protocol-v2 --insecure
+choria jwt prov credentials/server/provisioning.jwt credentials/issuer/issuer.seed --token s3cret --urls nats://broker.choria.local:4222 --default --protocol-v2 --insecure --update
 
 log "Creating provisioner credentials"
 choria jwt keys credentials/provisioner/signer.seed credentials/provisioner/signer.public
